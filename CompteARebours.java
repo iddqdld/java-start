0263db8b08711ccbd5463ddec28479cd8dc1ddd5
iddqdld <152985414+iddqdld@users.noreply.github.com>
2025-02-04 17:33:57 +0100

    Exo 1 et 2

---
+++ CompteARebours.java
@@ -0,0 +1,10 @@
+public class CompteARebours {
+    public static void main(String[] args) {
+        for (int x = 10; x >= 1; x --){
+            System.out.println(x);
+            if (x == 1){
+                System.out.println("Lancement !");
+            }
+        }
+    }
+}
---
+++ comptenbpair.java
@@ -0,0 +1,10 @@
+public class comptenbpair {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 20; x ++){
+            if (x % 2 == 0){
+                System.out.println(x);
+            }
+        }
+    }
+    
+}

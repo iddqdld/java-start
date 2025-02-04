219209f7c0584599afa8109ffcca4534a5608e24
iddqdld <152985414+iddqdld@users.noreply.github.com>
2025-02-04 17:35:57 +0100

    quick fix

--- CompteARebours.java
+++ CompteARebours.java
@@ -1,10 +1,8 @@
 public class CompteARebours {
     public static void main(String[] args) {
+        System.out.println("Lancement !");
         for (int x = 10; x >= 1; x --){
             System.out.println(x);
-            if (x == 1){
-                System.out.println("Lancement !");
-            }
         }
     }
 }

35f30806eddba96eb3e2354df133ad1911f667ce
iddqdld <152985414+iddqdld@users.noreply.github.com>
2025-02-18 17:24:09 +0100

    sql connexion

--- monsql-app/src/main/java/com/monprojet/App.java
+++ monsql-app/src/main/java/com/monprojet/App.java
@@ -1,13 +1,38 @@
 package com.monprojet;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    
+    // Informations de connexion
+    String url = "jdbc:mysql://localhost:3306/mydbjava"; // Remplacer "maBase" par le nom de votre base
+    String utilisateur = "root";
+    String motDePasse = "";
+    Connection connexion = null;
+    
+    try {
+        // Établir la connexion
+        connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
+        System.out.println("Connexion reussie !");
+        
+    } catch (SQLException e) {
+        System.out.println("Erreur de connexion : " + e.getMessage());
+    } finally { // Toujours fermer la connexion pour éviter les fuites de ressources 
+        if (connexion != null) { 
+            try { 
+                connexion.close(); 
+                System.out.println("Connexion fermee avec succes."); 
+            } catch (SQLException e) { 
+                System.err.println("Erreur lors de la fermeture de la connexion : " + e.getMessage()); 
+            } 
+        } 
+    }
+
     }
 }
